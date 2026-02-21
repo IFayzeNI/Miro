f6903a46c611266280ff40322b9603da1d0c5fa9
Alexandr <lokl85425@gmail.com>
2026-02-21 23:16:49 +0300

    feat(sessions): create useSessionStore with token validation and auth methods(login, logout, refresh)

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,7 +1,8 @@
 import createFetchClient from "openapi-fetch";
 import createClient from "openapi-react-query";
 import { CONFIG } from "@/shared/model/config";
-import type { ApiPaths } from "./schema";
+import type { ApiPaths, ApiSchemas } from "./schema";
+import { useSessionStore } from "../model/session";
 
 export const fetchClient = createFetchClient<ApiPaths>({
   baseUrl: CONFIG.API_BASE_URL,
@@ -9,13 +10,31 @@ export const fetchClient = createFetchClient<ApiPaths>({
 
 export const rqClient = createClient(fetchClient);
 
-// fetchClient.GET('/boards').then(res =>{
-//   if (res.data) {
-//     res.data
-//   }
-//   if (res.error) {
-//     res.error
-//   }
-// })
+export const publicFetchClient = createFetchClient<ApiPaths>({
+  baseUrl: CONFIG.API_BASE_URL,
+});
 
-// const res = rqClient.useQuery('get', '/boards');
+export const publicRqClient = createClient(publicFetchClient);
+
+fetchClient.use({
+  async onRequest({ request }) {
+    const token = await useSessionStore.getState().refreshToken();
+
+    if (token) {
+      request.headers.set("Authorization", `Bearer ${token}`);
+    } else {
+      return new Response(
+        JSON.stringify({
+          code: "NOT_AUTHORIZED",
+          message: "You are not authorized to access this resource",
+        } as ApiSchemas["Error"]),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+    }
+  },
+});
